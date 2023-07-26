23305b6af92b8b4e7176e39b1779adbc4c6302c6
Daniel Lancheros <danielcoding327@gmail.com>
2023-07-25 22:49:37 -0400

    added card,header,footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import './App.css';
+import { useState } from "react";
+import './styles/App.css';
 import PageLayout from './pagelayout/PageLayout';
 
 function App() {
--- src/pagelayout/PageLayout.jsx
+++ src/pagelayout/PageLayout.jsx
@@ -1,12 +1,14 @@
-import Header from "./Header";
-import Footer from "./Footer";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import EventCard from "../components/EventCard";
 
 export default function PageLayout ({ children }) {
 
   return(
     <main>
       <Header />
-      {children}
+      <EventCard />
+      
       <Footer />
     </main>
   )
